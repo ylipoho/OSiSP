d5082f43bc946d35bbc6c3e1c0e0c12ff6c53f34
ylipoho <crykaya@mail.ru>
2018-11-13 18:50:26 +0300

    done

--- OSISPSecondLab/OSISPSecondLab/Main.cpp
+++ OSISPSecondLab/OSISPSecondLab/Main.cpp
@@ -2,30 +2,39 @@
 #include <stdlib.h>  
 #include <tchar.h>
 #include <fstream>
+#include <string>
 
-#define ROWS 10
-#define COLUMNS 10
-#define MAX_ROWS 20
-#define MAX_COLUMNS 20
+#define ROWS 4
+#define COLUMNS 4
+#define MAX_ROWS 5
+#define MAX_COLUMNS 6
 #define MIN_ROWS 2
 #define MIN_COLUMNS 2
-#define FONT_NAME L"Times New Roman"
+#define SCROLL_LINE_WIDTH 25
+#define INDENT 10
+#define FONT_NAME L"Consolas"
 
 HDC hdc; 
-int windowWidth = 1000, windowHeight = 500;
+int windowWidth = 600, windowHeight = 200;
 int columns = COLUMNS, rows = ROWS;
-int mouseX, mouseY;
+int x, y;
+int columnWidth;
+int shiftNum = 0; // текущий сдвиг
+int tableHeight;
+int currentScrollPos = 0; // текущая позиция скролла
+int posNum = 1; // количество возможный в данный момент позиций
+int posPerPage; // примерное количество строк в странице
 
 PAINTSTRUCT ps; 
-HBRUSH backgroundBrush, lineBrush;
-COLORREF backgroundColor = RGB(241, 230, 220);
-COLORREF lineColor = RGB(166, 62, 20);
-RECT backgroundRect, partRect;
+HPEN linePen;
+HGDIOBJ prevPen;
+COLORREF backgroundColor = RGB(250, 250, 235);	// фон окна и текста
+COLORREF textColor = RGB(50, 50, 30);	//цвет текста 
+COLORREF penColor = RGB(80, 180, 70);	// цвет линий
 HMENU menu, hPopupMenu1, hPopupMenu2;
 FILE *file;
 
-
-int table[ROWS][COLUMNS];
+char* table[MAX_ROWS*MAX_COLUMNS];
 LOGFONT logFont;
 TEXTMETRIC textMetric;
 HFONT hFont;  
@@ -49,7 +58,7 @@ int CALLBACK WinMain(
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.hbrBackground = CreateSolidBrush(backgroundColor);
 	wcex.lpszMenuName = NULL;
 	wcex.lpszClassName = _T("WindowClass");
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
@@ -59,7 +68,7 @@ int CALLBACK WinMain(
 	HWND hWnd = CreateWindow(
 		_T("WindowClass"),
 		_T("Window Application"),
-		WS_OVERLAPPEDWINDOW,
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		HWND_DESKTOP,
@@ -90,7 +99,7 @@ int CALLBACK WinMain(
 	return 0;
 }
 
-void CreateMenu(HWND hWnd)        
+VOID CreateMenu(HWND hWnd)	// создание меню
 {
 	menu = CreateMenu();
 	hPopupMenu1 = CreatePopupMenu(); 
@@ -105,21 +114,30 @@ void CreateMenu(HWND hWnd)
 	SetMenu(hWnd, menu);
 }
 
-BOOL ReadFile()
+VOID ReadFile() // заполнение таблицы из файла
 {
-	std::ifstream in("C:\\Users\\admin\\source\\repos\\OSISPSecondLab\\NumsTable.txt");
-	if (!in.is_open())
-		return false;
-	for (int i = 0; i < ROWS; i++)
-		for (int j = 0; j < COLUMNS; j++)
-			in >> table[i][j];
-	in.close();
-	return true;
+	try
+	{
+		std::ifstream file("C:\\Users\\admin\\source\\repos\\OSISPSecondLab\\WordTable.txt");
+		if (file.is_open())
+		{
+			for (int i = 0; i < MAX_ROWS*MAX_COLUMNS; i++)
+			{
+				table[i] = new char[256];
+				file.getline(table[i], 256);
+			}
+			file.close();
+		}
+	}
+	catch (int i)
+	{
+		MessageBox(GetActiveWindow(), _T("Something bad happened..."), _T("Error"), MB_ICONERROR);
+	}
 }
 
-VOID InitializeFont()
+void InitializeFont()
 {
-	logFont.lfHeight = 20; 
+	logFont.lfHeight = 25; 
 	logFont.lfWidth = 0; 
 	logFont.lfEscapement = 0;
 	logFont.lfOrientation = 0; 
@@ -133,44 +151,206 @@ VOID InitializeFont()
 	logFont.lfQuality = PROOF_QUALITY;
 	logFont.lfPitchAndFamily = VARIABLE_PITCH | FF_MODERN; 
 	wcscpy_s(logFont.lfFaceName, FONT_NAME);
+	textMetric.tmAveCharWidth = 10;	
 	hFont = CreateFontIndirect(&logFont);
 }
-// DONE ==========================================================^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+void PrintTable(HWND hWnd) 
+{
+	columnWidth = (windowWidth/columns);
+	RECT cellRect;
+	y = 0;
+	int count = 0;
+
+	for (int i = 0; i < rows; i++)
+	{
+		int h = 1;
+		x = 0;
+		int hMax = 0;
+		SelectObject(hdc, linePen);
+		MoveToEx(hdc, 0, y + shiftNum + 1, NULL);
+		LineTo(hdc, columns*columnWidth, y + shiftNum + 1);
+
+		for (int j = 0; j < columns; j++)
+		{
+			SetRect(&cellRect, x, y + INDENT + shiftNum, x + columnWidth, y + 1000 + shiftNum);
+			SetBkColor(hdc, backgroundColor);
+			SelectObject(hdc, hFont);
+			SetTextColor(hdc, textColor);
+			h = DrawTextA(hdc, table[count], strlen(table[count]), &cellRect, DT_CENTER | DT_WORDBREAK | DT_EDITCONTROL);
+			count++;
+			x += columnWidth;
+			
+			if (h > hMax)
+				hMax = h;
+		}
+		
+		y += hMax+INDENT*2;
+
+		SelectObject(hdc, linePen);
+		MoveToEx(hdc, 0, y + shiftNum, NULL);
+		LineTo(hdc, columns*columnWidth, y + shiftNum);
+	}
+
+	MoveToEx(hdc, 0, y + shiftNum, NULL);
+	LineTo(hdc, columns*columnWidth, y + shiftNum);
+	tableHeight = y;
+	
+	x = 0;
+	for (int i = 0; i <= columns; i++) {
+		
+		SelectObject(hdc, linePen);
+		MoveToEx(hdc, x, 0 + shiftNum, NULL);
+		LineTo(hdc, x, y + shiftNum);
+		x += columnWidth;
+	}
+}
+
+int GetPosNum(HWND hWnd) // количество позиций
+{
+	if (windowHeight < tableHeight)
+	{
+		float d = ((float)(tableHeight - windowHeight) / (float)SCROLL_LINE_WIDTH) + 1;
+		return ceil(d);
+	}
+	else
+	{
+		currentScrollPos = 0;
+		shiftNum = 0;
+		return 1;
+	}
+}
+
+void RedrawIfChange(HWND hWnd, int prevPos)
+{
+	if (prevPos != currentScrollPos) // если позиция изменилась
+	{
+		SetScrollPos(hWnd, SB_VERT, currentScrollPos, TRUE);
+		InvalidateRect(hWnd, NULL, TRUE);
+	}
+}
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam; // сбщ, посылаемое окну, когда размер или позиция окнасобираются измениться
-	// Значение lParam. Указывает на структуру MINMAXINFO, которая содержит значения позиции и габариты разворачивания 
-	//по умолчанию и устанавливаемые по умолчанию минимальные и максимальные размеры. Прикладная программа может отменить 
-	//значения параметров по умолчанию элементовов этой структуры.
-
+	LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+	int wheelNum;
+	int prevPos, prevPosNum;
 	switch (message)
 	{
 	case WM_CREATE:
 		CreateMenu(hWnd);
 		InitializeFont();
 		ReadFile();
-		backgroundBrush = CreateSolidBrush(RGB(240, 250, 235));
-		lineBrush = CreateSolidBrush(RGB(62, 98, 94));
-		
+		linePen = CreatePen(PS_SOLID, 2, penColor);
+		prevPen = SelectObject(hdc, linePen);
+		currentScrollPos = 0;
+		shiftNum = 0;
+		SetScrollRange(hWnd, SB_VERT, 0, 0, FALSE);
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		wchar_t buf[10];
-		swprintf_s(buf, L"%d", table[1][2]);
-		TextOut(hdc, 10, 10, buf, _tcsclen(buf));
+		PrintTable(hWnd);
 		EndPaint(hWnd, &ps);
+
+		prevPosNum = posNum;
+		posNum = GetPosNum(hWnd);
+
+		SetScrollRange(hWnd, SB_VERT, 0, posNum-1, FALSE);
+		if (prevPosNum < posNum)
+		{
+			SetScrollPos(hWnd, SB_VERT, 0, TRUE);			
+		}
+		break;
+	case WM_GETMINMAXINFO:
+		lpMMI->ptMinTrackSize.x = 200;
+		lpMMI->ptMinTrackSize.y = 200;
 		break;
 	case WM_SIZE:
 		windowHeight = HIWORD(lParam);
 		windowWidth = LOWORD(lParam);
-		break;
-	case WM_GETMINMAXINFO:
+
+		posNum = GetPosNum(hWnd);
+		currentScrollPos = 0;
+		shiftNum = 0;
+		SetScrollPos(hWnd, SB_VERT, currentScrollPos, TRUE);
+		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_VSCROLL:
+		prevPos = currentScrollPos;
+		switch (wParam) {
+		case SB_PAGEUP:
+			posPerPage = windowHeight / SCROLL_LINE_WIDTH;
+			if (currentScrollPos - posPerPage <= 0)  // если сверху меньше, чем страница
+			{
+				currentScrollPos = 0;
+				shiftNum = 0;
+			}
+			else // если сверху еще больше, чем страница 
+			{	
+				currentScrollPos -= posPerPage;
+				shiftNum = -currentScrollPos * SCROLL_LINE_WIDTH;
+			}
+			SetScrollPos(hWnd, SB_VERT, currentScrollPos, TRUE);
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case SB_PAGEDOWN:
+			posPerPage = windowHeight / SCROLL_LINE_WIDTH;
+			if (currentScrollPos + posPerPage >= posNum - 1)  // если снизу меньше, чем страница
+			{
+				currentScrollPos = posNum - 1;
+			}
+			else // если снизу еще больше, чем страница 
+			{ 
+				currentScrollPos += posPerPage;
+			}
+			shiftNum = -currentScrollPos * SCROLL_LINE_WIDTH;
+			SetScrollPos(hWnd, SB_VERT, currentScrollPos, TRUE);
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case SB_LINEUP:	
+			if (currentScrollPos > 0 && posNum > 1)
+			{
+				currentScrollPos--;
+				shiftNum += SCROLL_LINE_WIDTH;
+			}
+			break;
+		case SB_LINEDOWN:
+			if (posNum > 1 && currentScrollPos < posNum-1)
+			{
+				currentScrollPos++;
+				shiftNum -= SCROLL_LINE_WIDTH;
+			}
+			break;
+		}
+		RedrawIfChange(hWnd, prevPos);
 		break;
-	case WM_COMMAND: // DONE ==========================================================vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+	case WM_MOUSEWHEEL:
+	{
+		prevPos = currentScrollPos;
+		wheelNum = GET_WHEEL_DELTA_WPARAM(wParam);
+
+		if (wheelNum > 0) // up
+		{
+			if (currentScrollPos > 0 && posNum > 1)
+			{
+				shiftNum += SCROLL_LINE_WIDTH;
+				currentScrollPos--;
+			}
+		}
+		else // down
+		{
+			if (posNum > 1 && currentScrollPos < posNum - 1)
+			{
+				shiftNum -= SCROLL_LINE_WIDTH; 
+				currentScrollPos++;
+			}
+		}
+		RedrawIfChange(hWnd, prevPos);
+	}
+	break;
+	case WM_COMMAND:
 		switch (wParam)
 		{
 		case 11:
@@ -190,11 +370,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				columns--;
 			break;
 		}
+		currentScrollPos = 0;
+		SetScrollPos(hWnd, SB_VERT, currentScrollPos, TRUE);
+		shiftNum = 0;
+		posNum = GetPosNum(hWnd);
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	case WM_DESTROY:
-		DeleteObject(backgroundBrush);
-		DeleteObject(lineBrush);
+		SelectObject(hdc, prevPen);
+		DeleteObject(linePen);
+		DeleteObject(hFont);
 		PostQuitMessage(0);
 		break;
 	default:
